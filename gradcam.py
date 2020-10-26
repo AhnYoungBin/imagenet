667a024467a6efff60a13a4b4cd70c7bd2d2404f
AhnYoungBin <49204525+AhnYoungBin@users.noreply.github.com>
2020-10-26 18:32:33 +0900

    Create gradcam.py

---
+++ gradcam.py
@@ -0,0 +1,33 @@
+from model import resnet
+import utils
+import cv2
+import numpy as np
+import argparse
+import torch
+
+parser = argparse.ArgumentParser(description='GradCAM')
+parser.add_argument('path',type=str, help='image path')
+args = parser.parse_args()
+
+
+model = resnet.ResNet50()
+image_path = args.path
+img = cv2.imread(image_path, 1)
+img = np.float32(cv2.resize(img, (224, 224))) / 255
+input = utils.preprocess_image(img)
+use_cuda = torch.cuda.is_available()
+target_index = None
+
+grad_cam = utils.GradCam(model=model, feature_module=model.layer4, \
+                       target_layer_names=["2"], use_cuda=use_cuda)
+mask = grad_cam(input, target_index)
+utils.show_cam_on_image(img, mask)
+gb_model = utils.GuidedBackpropReLUModel(model=model, use_cuda=use_cuda)
+gb = gb_model(input, index=target_index)
+gb = gb.transpose((1, 2, 0))
+cam_mask = cv2.merge([mask, mask, mask])
+cam_gb = utils.deprocess_image(cam_mask*gb)
+gb = utils.deprocess_image(gb)
+
+cv2.imwrite('gb.jpg', gb)
+cv2.imwrite('cam_gb.jpg', cam_gb)
